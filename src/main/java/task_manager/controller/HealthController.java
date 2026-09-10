6d084e97e280f4d2cad7363015cbef2ef8270437
medballa7 <medballa7@gmail.com>
2026-09-10 12:22:53 +0000

    Log Failed Smoke test

---
+++ src/main/java/task_manager/controller/HealthController.java
@@ -0,0 +1,15 @@
+package task_manager.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Application is healthy");
+    }
+}
